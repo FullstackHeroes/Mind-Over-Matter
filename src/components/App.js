a6dbf3a81542e27f3410cdd2f2dd3a52c99c0c50
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-30 11:46:11 -0500

    add Navbar, Home and Routes to comps/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import logo from "../images/logo.svg";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Routes from "../routes";
 
 function App() {
   return (
     <div className="App">
       <h1>Mind Over Matter</h1>
-        
+      <NavBar />
+      <Home />
+      <Routes />
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -19,19 +19,20 @@ class Routes extends Component {
     const { isLoggedIn } = this.props;
 
     return (
-      <Switch>
+      //commented this out until we decide on the structure of our project.
+      // <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
+        {/* <Route path="/login" component={Login} /> */}
+        {/* <Route path="/signup" component={Signup} /> */}
+        {/* {isLoggedIn && ( */}
+          {/* <Switch> */}
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={Home} />
-          </Switch>
-        )}
+            {/* <Route path="/home" component={Home} /> */}
+          {/* </Switch> */}
+        {/* )} */}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
-      </Switch>
+        {/* <Route component={Login} /> */}
+      // </Switch>
     );
   }
 }
