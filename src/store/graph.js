fa9fac70611bb27144eb6ca3848a1206ba7cccc3
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-30 06:37:24 -0500

    add graph store stuff

---
+++ src/store/graph.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+//We are probably going to have more than one type of graph
+//Just threw a quick template together so that we have it 
+
+
+/**
+ * ACTION TYPES
+ */
+const GET_GRAPH = 'GET_GRAPH'
+const REMOVE_GRAPH = 'REMOVE_GRAPH'
+
+/**
+ * INITIAL STATE
+ */
+const defaultGraph = {}
+
+/**
+ * ACTION CREATORS
+ */
+const gotGraph = graph => ({type: GET_GRAPH, graph})
+const removeGraph = () => ({type: REMOVE_GRAPH})
+
+/**
+Thunks
+ */
+
+
+export const getAGraph = function(id) {
+  return async dispatch => {
+    // const { data } = Async call here 
+    dispatch(gotGraph(data))
+  }
+}
+
+//REDUCERS
+
+export default function flowerReducer(graph = {}, action) {
+  switch (action.type) {
+    case GET_GRAPH:
+      return {/* GraphStuff*/}
+    case REMOVE_GRAPH:
+      return {/* GraphStuff*/}
+    default:
+      return graph
+  }
+}
