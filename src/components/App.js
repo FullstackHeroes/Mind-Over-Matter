c019f607be9c8c84df9047e7879b0f83aecd3050
Kenneth Chen <kennethchen91@gmail.com>
2020-01-30 09:43:39 -0500

    set up firebase config

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
-import React from "react";
-import logo from "../images/logo.svg";
+import React from 'react';
+import logo from '../images/logo.svg';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 
 function App() {
+  var firebaseConfig = {
+    apiKey: 'AIzaSyAge0F4hnrIf5lOXb4hW8CvZwCqgZqoG9M',
+    authDomain: 'mind-over-matter-5ee6a.firebaseapp.com',
+    databaseURL: 'https://mind-over-matter-5ee6a.firebaseio.com',
+    projectId: 'mind-over-matter-5ee6a',
+    storageBucket: 'mind-over-matter-5ee6a.appspot.com',
+    messagingSenderId: '536926014550',
+    appId: '1:536926014550:web:e446bad28a88b1f984fff7',
+    measurementId: 'G-ETK41B9X1V',
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
