02a58562fa164190084e5eec2014fd0d81620033
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-30 06:26:51 -0500

    remove unecessary code from store/user.js

--- src/store/user.js
+++ src/store/user.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import history from '../history'
+
 
 /**
  * ACTION TYPES
@@ -23,7 +23,7 @@ const removeUser = () => ({type: REMOVE_USER})
  */
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get('/auth/me')
+    //going to need some async here
     dispatch(getUser(res.data || defaultUser))
   } catch (err) {
     console.error(err)
@@ -33,24 +33,23 @@ export const me = () => async dispatch => {
 export const auth = (email, password, method) => async dispatch => {
   let res
   try {
-    res = await axios.post(`/auth/${method}`, {email, password})
+       //going to need some async here
   } catch (authError) {
     return dispatch(getUser({error: authError}))
   }
 
   try {
     dispatch(getUser(res.data))
-    history.push('/home')
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr) //this is just a fancy way to label the error 
   }
 }
 
 export const logout = () => async dispatch => {
   try {
-    await axios.post('/auth/logout')
+    //going to need some async here
     dispatch(removeUser())
-    history.push('/login')
+  
   } catch (err) {
     console.error(err)
   }
