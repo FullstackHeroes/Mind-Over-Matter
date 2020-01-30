e985ede82414017fda0d57bb806efb2e7d1bffd7
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-30 06:54:07 -0500

    add navbar comp, taken layput from boilermaker, commented things out that we may not need until later

---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+//Straight up jacked this from boilermaker
+//I commented out some links below until we decide to use them.
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+// import {logout} from '../store' //Currently we dont need this 
+
+const Navbar = ({handleClick, isLoggedIn}) => (
+  <div>
+    <h1>Hello M8!</h1>
+    <nav>
+      {isLoggedIn ? (
+        <div>
+          {/* The navbar will show these links after you log in */}
+          {/* <Link to="/home">Home</Link> */}
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div>
+          {/* The navbar will show these links before you log in */}
+          {/* <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link> */}
+        </div>
+      )}
+    </nav>
+    <hr />
+  </div>
+)
+
+/**
+ * CONTAINER
+ */
+const mapState = state => {
+  return {
+    // isLoggedIn: !!state.user.id
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    handleClick() {
+      dispatch(logout())
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(Navbar)
+
+/**
+ * PROP TYPES
+ * Not sure exactly how these work, I think its worth looking into
+ */
+Navbar.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired
+}
