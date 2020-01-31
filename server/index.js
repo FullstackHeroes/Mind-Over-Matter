c8a6dcfa83e9114ac07efea36ff02f771fe8bd04
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-31 12:51:42 -0500

    add basic server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const app = express();
+const path = require("path");
+const express = require("express");
+const volleyball = require("volleyball");
+// const db = require('./db') //for when db is up
+module.exports = app;
+
+app.use(volleyball);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", require("./api"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Server Error");
+});
