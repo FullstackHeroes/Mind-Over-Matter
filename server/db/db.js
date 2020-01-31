e7b6c1462d2760524e0cb4388d28a07b7d6a968a
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-31 13:22:42 -0500

    add module export for pg-node backend

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,10 @@
 const { Pool, Client } = require("pg");
 
 const pool = new Pool();
+
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  },
+}
