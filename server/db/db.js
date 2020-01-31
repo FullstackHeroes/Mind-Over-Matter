62bc2ccac98ab64ace94d6f6173d53e11c26a4a8
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-31 14:32:32 -0500

    progress

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,9 @@ const { Pool, Client } = require("pg");
 
 const pool = new Pool();
 
-
 module.exports = {
   query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
+    return pool.query(text, params, callback);
+  }
+  //all of our routes and quieries should go here. This is an exceellent place to debug.
+};
