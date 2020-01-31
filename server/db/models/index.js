b6815c5899da04a60e56ff99b6d95abad07a20fe
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-01-31 16:28:02 -0500

    models index ready, commented out until models are ready

---
+++ server/db/models/index.js
@@ -0,0 +1,12 @@
+//until we have proper exports these will break the code.
+// const db = require("../db");
+// const { Hour, Day, Week } = require ("./score")
+
+
+
+// module.exports = {
+//   db,
+//   Hour,
+//   Day,
+//   Week
+// };
