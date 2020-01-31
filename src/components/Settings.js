eba9e4049f9476e17a1b407681b8ff3878fd6aeb
James Shen <jjss886@gmail.com>
2020-01-30 23:11:15 -0500

    refactored certain code structures

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 
 class Settings extends Component {
   constructor() {
--- src/store/user.js
+++ src/store/user.js
@@ -1,70 +1,60 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-/**
- * ACTION TYPES
- */
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+// INITIAL STATE
+const initialState = {
+  name: ""
+};
 
-/**
- * INITIAL STATE
- */
-const defaultUser = {
-  name : ""
-}
+// ACTION TYPES
+const GET_USER = "GET_USER";
+const REMOVE_USER = "REMOVE_USER";
 
-/**
- * ACTION CREATORS
- */
-const getUser = user => ({type: GET_USER, user})
-const removeUser = () => ({type: REMOVE_USER})
+// ACTION CREATORS
+export const getUser = user => ({type: GET_USER, user});
+export const removeUser = () => ({type: REMOVE_USER});
 
-/**
- * THUNK CREATORS
- */
+// THUNKY THUNKS
 export const me = () => async dispatch => {
   try {
     // const res = await axios.get('/auth/me')
     // dispatch(getUser(res.data || defaultUser))
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export const auth = (email, password, method) => async dispatch => {
-  let res
+  // let res;
   try {
     // res = await axios.post(`/auth/${method}`, {email, password})
   } catch (authError) {
-    return dispatch(getUser({error: authError}))
+    return dispatch(getUser({error: authError}));
   }
 
   try {
     // dispatch(getUser(res.data))
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr);
   }
-}
+};
 
 export const logout = () => async dispatch => {
   try {
     // await axios.post('/auth/logout')
     // dispatch(removeUser())
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
-/**
- * REDUCER
- */
-export default function(state = defaultUser, action) {
+// REDUCER
+export default function(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return {};
     default:
-      return state
+      return state;
   }
 }
