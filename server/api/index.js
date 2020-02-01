4b033b81f2b44aa3008e78be3867b7b9603ffa77
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-01 16:56:56 -0500

    add error oute to api/index

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+router.use("/users", require("./users"));
+router.use("/scores", require("./scores"));
+
+router.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
 module.exports = router;
