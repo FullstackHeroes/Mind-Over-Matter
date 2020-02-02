b27cde4459b753a5b241a57c53525acb367d1bd2
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-02 03:17:38 -0500

    add SignIn comp, still needs work

---
+++ src/components/SignIn.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class LogIn extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      firstName: "",
+      lastName: "",
+      password: ""
+    };
+  }
+  render() {
+    return <div>{/* Form data goes here */}</div>;
+  }
+}
