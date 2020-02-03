45eea5a1c012c53c2132a515204ecc15542b36e3
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-03 16:27:30 -0500

    all routes working

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,72 @@
 const router = require("express").Router();
+const { User } = require("../db/models");
 const db = require("../db");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      attributes: ["id", "email"]
+    });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      email: req.body.email,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: req.body.password
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(+req.params.id);
+    if (!user) return res.status(404).json("No such user at our store!");
+    const updatedUser = await User.update(req.body, {
+      where: { id: req.params.id },
+      returning: true,
+      plain: true
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await User.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
 
