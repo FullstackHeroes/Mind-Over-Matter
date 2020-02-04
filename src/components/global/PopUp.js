bae12ca8316e161fafcd15216b42a49854bf35d5
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-04 12:11:23 -0500

    create popup comp

---
+++ src/components/global/PopUp.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PopUp extends React.Component {
+  render() {
+    return (
+      <div className="popup">
+        <div className="popup\_inner">
+          <h1>{this.props.text}</h1>
+          <button onClick={this.props.closePopup}>close me</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PopUp;
