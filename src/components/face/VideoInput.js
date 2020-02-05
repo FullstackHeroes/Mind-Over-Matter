396ba91f4b337d8c8564301da77a8776657ff36b
James Shen <jjss886@gmail.com>
2020-02-05 18:02:16 -0500

    fixed posting in the normalizeScore

--- src/components/face/VideoInput.js
+++ src/components/face/VideoInput.js
@@ -109,7 +109,6 @@ class VideoInput extends Component {
                 );
 
               //THE TRIGGER TO SHOW THE HELP MODAL
-              console.log("percent diff:", perDiff);
               if (perDiff <= 45) {
                 this.setState({
                   emoPercent: perDiff
