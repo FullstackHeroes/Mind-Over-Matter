e18f4138db6d235591d8924de4655629cbd75770
James Shen <jjss886@gmail.com>
2020-02-05 22:24:12 -0500

    created basis case for normalizescore backend

--- server/api/normalizeScore.js
+++ server/api/normalizeScore.js
@@ -11,26 +11,28 @@ router.get("/:userId", async function(req, res, next) {
       normalizeScoreArr = [],
       runningScoreArr = [],
       sentimentDiffArr = [];
-    allNormalizeScores.map(entry => {
-      const normalizeScoreObj = {},
-        runningScoreObj = {},
-        sentimentDiffObj = {};
-      normalizeScoreObj.id = entry.id;
-      normalizeScoreObj.normalizeScore = entry.normalizeScore;
-      normalizeScoreObj.timeStamp = entry.timeStamp;
-      normalizeScoreObj.userId = entry.userId;
-      runningScoreObj.id = entry.id;
-      runningScoreObj.runningScore = entry.runningScore;
-      runningScoreObj.timeStamp = entry.timeStamp;
-      runningScoreObj.userId = entry.userId;
-      sentimentDiffObj.id = entry.id;
-      sentimentDiffObj.sentimentDiff = entry.sentimentDiff;
-      sentimentDiffObj.timeStamp = entry.timeStamp;
-      sentimentDiffObj.userId = entry.userId;
-      normalizeScoreArr.push(normalizeScoreObj);
-      runningScoreArr.push(runningScoreObj);
-      sentimentDiffArr.push(sentimentDiffObj);
-    });
+    if (allNormalizeScores.length) {
+      allNormalizeScores.map(entry => {
+        const normalizeScoreObj = {},
+          runningScoreObj = {},
+          sentimentDiffObj = {};
+        normalizeScoreObj.id = entry.id;
+        normalizeScoreObj.normalizeScore = entry.normalizeScore;
+        normalizeScoreObj.timeStamp = entry.timeStamp;
+        normalizeScoreObj.userId = entry.userId;
+        runningScoreObj.id = entry.id;
+        runningScoreObj.runningScore = entry.runningScore;
+        runningScoreObj.timeStamp = entry.timeStamp;
+        runningScoreObj.userId = entry.userId;
+        sentimentDiffObj.id = entry.id;
+        sentimentDiffObj.sentimentDiff = entry.sentimentDiff;
+        sentimentDiffObj.timeStamp = entry.timeStamp;
+        sentimentDiffObj.userId = entry.userId;
+        normalizeScoreArr.push(normalizeScoreObj);
+        runningScoreArr.push(runningScoreObj);
+        sentimentDiffArr.push(sentimentDiffObj);
+      });
+    }
     res.json({ normalizeScoreArr, runningScoreArr, sentimentDiffArr });
   } catch (err) {
     next(err);
@@ -61,26 +63,28 @@ router.post("/", async function(req, res, next) {
       normalizeScoreArr = [],
       runningScoreArr = [],
       sentimentDiffArr = [];
-    allNormalizeScores.map(entry => {
-      const normalizeScoreObj = {},
-        runningScoreObj = {},
-        sentimentDiffObj = {};
-      normalizeScoreObj.id = entry.id;
-      normalizeScoreObj.normalizeScore = entry.normalizeScore;
-      normalizeScoreObj.timeStamp = entry.timeStamp;
-      normalizeScoreObj.userId = entry.userId;
-      runningScoreObj.id = entry.id;
-      runningScoreObj.runningScore = entry.runningScore;
-      runningScoreObj.timeStamp = entry.timeStamp;
-      runningScoreObj.userId = entry.userId;
-      sentimentDiffObj.id = entry.id;
-      sentimentDiffObj.sentimentDiff = entry.sentimentDiff;
-      sentimentDiffObj.timeStamp = entry.timeStamp;
-      sentimentDiffObj.userId = entry.userId;
-      normalizeScoreArr.push(normalizeScoreObj);
-      runningScoreArr.push(runningScoreObj);
-      sentimentDiffArr.push(sentimentDiffObj);
-    });
+    if (allNormalizeScores.length) {
+      allNormalizeScores.map(entry => {
+        const normalizeScoreObj = {},
+          runningScoreObj = {},
+          sentimentDiffObj = {};
+        normalizeScoreObj.id = entry.id;
+        normalizeScoreObj.normalizeScore = entry.normalizeScore;
+        normalizeScoreObj.timeStamp = entry.timeStamp;
+        normalizeScoreObj.userId = entry.userId;
+        runningScoreObj.id = entry.id;
+        runningScoreObj.runningScore = entry.runningScore;
+        runningScoreObj.timeStamp = entry.timeStamp;
+        runningScoreObj.userId = entry.userId;
+        sentimentDiffObj.id = entry.id;
+        sentimentDiffObj.sentimentDiff = entry.sentimentDiff;
+        sentimentDiffObj.timeStamp = entry.timeStamp;
+        sentimentDiffObj.userId = entry.userId;
+        normalizeScoreArr.push(normalizeScoreObj);
+        runningScoreArr.push(runningScoreObj);
+        sentimentDiffArr.push(sentimentDiffObj);
+      });
+    }
     res.json({ normalizeScoreArr, runningScoreArr, sentimentDiffArr });
   } catch (err) {
     next(err);
