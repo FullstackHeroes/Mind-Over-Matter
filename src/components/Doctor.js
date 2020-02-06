d7baec02df1e9447b337cbbbb8017cded2c12b2d
Kenneth Chen <kennethchen91@gmail.com>
2020-02-06 12:36:10 -0500

    moved calculations to popup.js and made alertcontents.js

---
+++ src/components/Doctor.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+const Doctor = () => (
+  <div>
+    <div align="center">
+      <h1>Find A Doctor</h1>
+    </div>
+    <div></div>
+  </div>
+);
+
+export default Doctor;
--- src/components/global/PopUp.js
+++ src/components/global/PopUp.js
@@ -1,81 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import AlertContent from "./AlertContent";
+import { setNormalizedScore } from "../../store";
 
-export default class PopUp extends Component {
-  onClose = () => {
-    this.props.onClose && this.props.onClose();
+class PopUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopUp: false,
+      emoPercent: 0,
+      lastAlert: new Date()
+    };
+    this.showHelp = this.showHelp.bind(this);
+    this.hideHelp = this.hideHelp.bind(this);
+  }
+
+  showHelp = () => {
+    this.setState({ showPopUp: true });
   };
 
-  helpMessage = status => {
-    if (status <= 45) {
-      return (
-        <div>
-          <div>
-            <h5>Woah there Buddy!</h5>
-            <h5>Step away from your desk and put those hands up!</h5>
-            <h5>
-              Seems to us like you have had quite the day... you deserve a walk
-            </h5>
-            <h5>Take 10-15 mins to yourself and walk it off</h5>
-          </div>
-          <div>
-            <img
-              src={
-                " https://sd.keepcalms.com/i/keep-calm-and-step-away-from-the-computer.png"
-              }
-            />
-          </div>
-        </div>
-      );
-    } else if (status <= 55) {
-      return (
-        <div>
-          <div>
-            <h4>Looking Steamy!</h4>
-            <h4>Stretch those legs and go hang out with your buddy Joe!</h4>
-          </div>
-          <div>
-            <img
-              src={
-                "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
-              }
-            />
-          </div>
-        </div>
-      );
-    } else if (status <= 65) {
-      return (
-        <div>
-          <div>
-            <h4>Here is a cute picture of a cat!</h4>
-            <h4>Hope it makes your day!</h4>
-          </div>
-          <div>
-            <img
-              src={
-                "https://www.mobilecause.com/wp-content/uploads/2016/03/shutterstock_84271234.jpg>"
-              }
-            />
-          </div>
-        </div>
-      );
-    } else if (status <= 80) {
-      return (
-        <div>
-          <div>
-            <h5>Word on the streets is that you think better when hydrated!</h5>
-            <h5>I think its time you grabbed a drink of water</h5>
-            <h5>We would give you ours but its stuck behind this screen</h5>
-          </div>
-          <div>
-            <img
-              src={
-                "https://www.sciencenews.org/wp-content/uploads/2018/11/112418_water_feat_free.jpg"
-              }
-            />
-          </div>
-        </div>
-      );
+  hideHelp = () => {
+    this.setState({ showPopUp: false });
+  };
+
+  //USER DATA AND CALCULATIONS
+  calcNormScore = async () => {
+    const { normalizedScore } = this.props,
+      mostRecentNormalized = normalizedScore[0].normalizeScore,
+      RunningTrueScore = await calcWeightedTrueScore(userId),
+      perDiff = percentDifference(RunningTrueScore, mostRecentNormalized);
+
+    //THE TRIGGER TO SHOW THE HELP ALERT
+    const checkDate = new Date();
+    // 15 mins in milisecs: 900000
+
+    if (
+      checkDate - this.state.lastAlert > 10000 &&
+      this.state.emoPercent <= 80
+    ) {
+      this.setState({
+        emoPercent: perDiff * 100
+      });
+      this.showHelp();
     }
   };
 
@@ -86,20 +52,20 @@ export default class PopUp extends Component {
 
     return (
       <div>
-        <h2>Hey! How's it goin?</h2>
-        <div>{this.helpMessage(helpStatus)}</div>
-        <button
-          style={{
-            position: "absolute",
-            bottom: 0
-          }}
-          onClick={this.onClose}>
-          close
-        </button>
+        <AlertContent onClose={this.showHelp} />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    normalizedScore: state.score.normalizedScore
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopUp);
 
 //spitballing css
 // .modal {
