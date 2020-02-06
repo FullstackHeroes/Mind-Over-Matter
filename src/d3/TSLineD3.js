4721cbe932f4f25e240f965244895655fecd55d5
James Shen <jjss886@gmail.com>
2020-02-06 13:08:59 -0500

    testing around with getting line chart to appear appropriately

--- src/d3/TSLineD3.js
+++ src/d3/TSLineD3.js
@@ -19,7 +19,7 @@ class TSLineD3 {
       .append("g")
       .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
-    // SCALE
+    // AXIS SCALE
     vis.x = d3.scaleLinear().range([0, WIDTH]);
     vis.y = d3.scaleLinear().range([HEIGHT, 0]);
 
@@ -63,10 +63,11 @@ class TSLineD3 {
     console.log("D3 UPDATING!", vis.data, xAttr, yAttr);
 
     // ADJUST SCALING
-    vis.x.domain([
-      d3.min(vis.data, d => Number(d[xAttr]) * 0.9),
-      d3.max(vis.data, d => Number(d[xAttr]) * 1.05)
-    ]);
+    // vis.x.domain([
+    //   d3.min(vis.data, d => Number(d[xAttr]) * 0.9),
+    //   d3.max(vis.data, d => Number(d[xAttr]) * 1.05)
+    // ]);
+    // vis.x.domain(d3.extent(vis.data, d => d[xAttr]));
     vis.y.domain([
       d3.min(vis.data, d => Number(d[yAttr]) * 0.95),
       d3.max(vis.data, d => Number(d[yAttr]) * 1.05)
@@ -79,52 +80,57 @@ class TSLineD3 {
     vis.xAxisGroup.transition(1000).call(xAxisCall);
     vis.yAxisGroup.transition(1000).call(yAxisCall);
 
-    // LINE CHART
+    // LINE CHART FUNCTION AND APPENDING
+    const valueLine = d3
+      .line()
+      .x((d, i) => {
+        console.log("PLEASE 1 -", d, d[xAttr]);
+        return vis.x(i);
+        // return vis.x;
+        // return vis.x(d[xAttr]);
+      })
+      .y(d => {
+        console.log("PLEASE 2 -", d, d[yAttr]);
+        return vis.y(d[yAttr]);
+        // return vis.y;
+      })
+      .curve(d3.curveMonotoneX);
+
     vis.lineChart = vis.g
       .append("path")
-      .datum(vis.data)
-      // .datum(vis.data, d => {
-      //   console.log("HMM -", d);
-      //   d.runningScore;
-      // })
       .attr("class", "runningScoreLine")
-      .attr(
-        "d",
-        d3
-          .line(vis.data)
-          .x(d => d[xAttr])
-          .y(d => d[yAttr])
-          .curve(d3.curveMonotoneX)
-      );
+      .attr("d", valueLine(vis.data));
 
-    // JOIN
-    const circles = vis.g.selectAll("circle").data(vis.data, d => d.name);
+    // // JOIN
+    // const circles = vis.g
+    //   .selectAll("circle")
+    //   .data(vis.data, d => d.runningScore);
 
-    // EXIT
-    circles
-      .exit()
-      .transition(1000)
-      .attr("cy", vis.y(0))
-      .remove();
+    // // EXIT
+    // circles
+    //   .exit()
+    //   .transition(1000)
+    //   .attr("cy", vis.y(0))
+    //   .remove();
 
-    // UPDATE
-    circles
-      .transition(1000)
-      .attr("cx", d => vis.x(d[xAttr]))
-      .attr("cy", d => vis.y(d[yAttr]));
+    // // UPDATE
+    // circles
+    //   .transition(1000)
+    //   .attr("cx", d => vis.x(d[xAttr]))
+    //   .attr("cy", d => vis.y(d[yAttr]));
 
-    // ENTER
-    circles
-      .enter()
-      .append("circle")
-      .classed("scatterCircle", true)
-      .attr("cy", vis.y(0))
-      .attr("cx", d => vis.x(d[xAttr]))
-      .attr("r", 9)
-      // .on("click", d => store.dispatch(setActiveName(d.name)))
-      .on("click", d => console.log("Hello Click!"))
-      .transition(1000)
-      .attr("cy", d => vis.y(d[yAttr]));
+    // // ENTER
+    // circles
+    //   .enter()
+    //   .append("circle")
+    //   .classed("scatterCircle", true)
+    //   .attr("cy", vis.y(0))
+    //   .attr("cx", d => vis.x(d[xAttr]))
+    //   .attr("r", 9)
+    //   // .on("click", d => store.dispatch(setActiveName(d.name)))
+    //   .on("click", d => console.log("Clicking -", d))
+    //   .transition(1000)
+    //   .attr("cy", d => vis.y(d[yAttr]));
   }
 }
 
