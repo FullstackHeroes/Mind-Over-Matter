45e2862ca6efc438528d9bf904930472c5c889b5
Robert Costello <robertcostello81@gmail.com>
2020-02-07 15:34:49 -0500

    survey

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,56 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
-class Survey extends Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label>
-              <input type="radio"></input>
-              <small>Great</small>
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="radio"></input>
-              <small>Good</small>
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="radio"></input>
-              <small>OK</small>
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="radio"></input>
-              <small>Not Good</small>
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="radio"></input>
-              <small>Bad</small>
-            </label>
-          </div>
-          <div>
-            <label>
-              <input type="submit"></input>
-              <small>Submit</small>
-            </label>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+import { checkPropTypes } from "prop-types";
+const Survey = props => {
+  const { user, handleSubmit } = props;
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label>
+            <input type="radio"></input>
+            <small>Great</small>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="radio"></input>
+            <small>Good</small>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="radio"></input>
+            <small>OK</small>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="radio"></input>
+            <small>Not Good</small>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="radio"></input>
+            <small>Bad</small>
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="submit"></input>
+            <small>Submit</small>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
     user: state.user
   };
+};
+
+Survey.prototypes = {
+  handleSubmit: PropTypes.func.isRequired
 };
 
 export default connect(mapStateToProps)(Survey);
