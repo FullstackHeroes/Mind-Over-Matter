096fab038244b1ec5d4062ac1f22b26e6de07fa4
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-08 19:30:40 -0500

    import function from utilis/utilities

--- src/components/data/Table.js
+++ src/components/data/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { setFullScoreObj } from "../../store";
 import TableRow from "./TableRow";
+import { makeCsvTable } from "../../utils/utilities";
 
 class Table extends Component {
   componentDidMount() {
