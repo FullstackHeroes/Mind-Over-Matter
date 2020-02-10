a8eedcea4b0ecdf7dd46598fe6243e2ffed9923d
James Shen <jjss886@gmail.com>
2020-02-10 12:50:16 -0500

    initial clean up of alert functionality

--- src/components/alert/AlertContent.js
+++ src/components/alert/AlertContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { dateCreate } from "../../utils/utilities";
+import AlertMessage from "./AlertMessage";
 
 class AlertContent extends Component {
   onClose = () => {
@@ -11,102 +12,105 @@ class AlertContent extends Component {
     console.log("help msg - ", status, currentDate - this.props.lastAlert);
 
     if (currentDate - this.props.lastAlert > 10000) {
-      if (status <= 65) {
-        return (
-          <div className="alertBackdrop d-flex justify-content-center">
-            <div className="alertContainer">
-              <div className="modal-dialog">
-                <h6>
-                  Woah there Buddy! Step away from your desk and put those hands
-                  up! Take 10-15 mins to yourself and walk it off
-                </h6>
-              </div>
-              <div className="alertImgDiv">
-                <img
-                  className="alertImg3"
-                  src={
-                    "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
-                    // "https://ctl.s6img.com/society6/img/7YUzbWKPlRNoDuosqB_FyQbfYag/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/8802652faa0d4b5a935d15a2a56436cc/~~/famous-band-walk-road-sign-design-prints.jpg"
-                  }
-                />
-              </div>
-              <div className="modal-footer d-flex justify-content-center">
-                <button
-                  className="align-self-center btn btn-secondary"
-                  onClick={this.onClose}>
-                  Got it
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      } else if (status <= 70) {
-        return (
-          <div className="alertBackdrop d-flex justify-content-center">
-            <div className="alertContainer">
-              <div className="modal-dialog">
-                <h6>
-                  Looking Steamy! Stretch those legs and go hang out with your
-                  buddy Cuppa Joe!
-                </h6>
-              </div>
-              <div className="alertImgDiv">
-                <img
-                  className="alertImg3"
-                  src={
-                    "https://i.pinimg.com/236x/fe/e9/e0/fee9e0741c5199b830273f88a6c4f8f3--coffee-cafe-cup-of-coffee.jpg"
-                    // "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
-                  }
-                />
-              </div>
-              <div className="modal-footer d-flex justify-content-center">
-                <button
-                  className="align-self-center btn btn-secondary"
-                  onClick={this.onClose}>
-                  Got it
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      } else if (status <= 78) {
-        console.log("HELLO ALERT");
-        return (
-          <div className="alertBackdrop d-flex justify-content-center">
-            <div className="alertContainer">
-              <div className="modal-dialog">
-                <h6>Here is a cute picture, Hope it makes your day!</h6>
-              </div>
-              <div className="alertImgDiv">
-                <img
-                  className="alertImg3"
-                  src={
-                    "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
-                    // "https://www.smallbizgenius.net/wp-content/uploads/2019/11/smallbizgenius_favicon.png"
-                  }
-                />
-              </div>
-              <div className="modal-footer d-flex justify-content-center">
-                <button
-                  className="align-self-center btn btn-secondary"
-                  onClick={this.onClose}>
-                  Got it
-                </button>
-              </div>
-            </div>
-          </div>
-        );
-      }
-      // else if (
-      //   this.props.threeHourSnapCount >=
-      //   (this.props.snapInterval / 1000) * 3600
-      // ) {
-      //   return <div>{this.props.snapInterval}</div>;
-      // }
-      else {
-        return null;
-      }
-    } else return null;
+      <AlertMessage status={status} onClose={this.onClose} />;
+    }
+    // if (currentDate - this.props.lastAlert > 10000) {
+    //   if (status <= 65) {
+    //     return (
+    //       <div className="alertBackdrop d-flex justify-content-center">
+    //         <div className="alertContainer">
+    //           <div className="modal-dialog">
+    //             <h6>
+    //               Woah there Buddy! Step away from your desk and put those hands
+    //               up! Take 10-15 mins to yourself and walk it off
+    //             </h6>
+    //           </div>
+    //           <div className="alertImgDiv">
+    //             <img
+    //               className="alertImg3"
+    //               src={
+    //                 "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
+    //                 // "https://ctl.s6img.com/society6/img/7YUzbWKPlRNoDuosqB_FyQbfYag/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/8802652faa0d4b5a935d15a2a56436cc/~~/famous-band-walk-road-sign-design-prints.jpg"
+    //               }
+    //             />
+    //           </div>
+    //           <div className="modal-footer d-flex justify-content-center">
+    //             <button
+    //               className="align-self-center btn btn-secondary"
+    //               onClick={this.onClose}>
+    //               Got it
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   } else if (status <= 70) {
+    //     return (
+    //       <div className="alertBackdrop d-flex justify-content-center">
+    //         <div className="alertContainer">
+    //           <div className="modal-dialog">
+    //             <h6>
+    //               Looking Steamy! Stretch those legs and go hang out with your
+    //               buddy Cuppa Joe!
+    //             </h6>
+    //           </div>
+    //           <div className="alertImgDiv">
+    //             <img
+    //               className="alertImg3"
+    //               src={
+    //                 "https://i.pinimg.com/236x/fe/e9/e0/fee9e0741c5199b830273f88a6c4f8f3--coffee-cafe-cup-of-coffee.jpg"
+    //                 // "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
+    //               }
+    //             />
+    //           </div>
+    //           <div className="modal-footer d-flex justify-content-center">
+    //             <button
+    //               className="align-self-center btn btn-secondary"
+    //               onClick={this.onClose}>
+    //               Got it
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   } else if (status <= 78) {
+    //     console.log("HELLO ALERT");
+    //     return (
+    //       <div className="alertBackdrop d-flex justify-content-center">
+    //         <div className="alertContainer">
+    //           <div className="modal-dialog">
+    //             <h6>Here is a cute picture, Hope it makes your day!</h6>
+    //           </div>
+    //           <div className="alertImgDiv">
+    //             <img
+    //               className="alertImg3"
+    //               src={
+    //                 "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
+    //                 // "https://www.smallbizgenius.net/wp-content/uploads/2019/11/smallbizgenius_favicon.png"
+    //               }
+    //             />
+    //           </div>
+    //           <div className="modal-footer d-flex justify-content-center">
+    //             <button
+    //               className="align-self-center btn btn-secondary"
+    //               onClick={this.onClose}>
+    //               Got it
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     );
+    //   }
+    //   // else if (
+    //   //   this.props.threeHourSnapCount >=
+    //   //   (this.props.snapInterval / 1000) * 3600
+    //   // ) {
+    //   //   return <div>{this.props.snapInterval}</div>;
+    //   // }
+    //   else {
+    //     return null;
+    //   }
+    // } else return null;
   };
 
   render() {
---
+++ src/components/alert/AlertMessage.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const AlertMessage = ({ status, onClose }) => {
+  console.log("alert msg -", status);
+  const tiers = [65, 70, 80];
+
+  const alertOptions = {
+    0: {
+      message:
+        "Woah there Buddy! Step away from your desk and put those hands up! Take 10-15 mins to yourself and walk it off",
+      image:
+        "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
+    },
+    1: {
+      message:
+        "Looking Steamy! Stretch those legs and go hang out with your buddy Cuppa Joe!",
+      image:
+        "https://i.pinimg.com/236x/fe/e9/e0/fee9e0741c5199b830273f88a6c4f8f3--coffee-cafe-cup-of-coffee.jpg"
+    },
+    2: {
+      message: "Here is a cute picture, Hope it makes your day!",
+      image:
+        "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
+    }
+  };
+
+  tiers.map((tier, idx) => {
+    if (status <= tier) {
+      return (
+        <div className="alertBackdrop d-flex justify-content-center">
+          <div className="alertContainer">
+            <div className="modal-dialog">
+              <h6>{alertOptions[idx].message}</h6>
+            </div>
+
+            <div className="alertImgDiv">
+              <img className="alertImg3" src={alertOptions[idx].image} />
+            </div>
+
+            <div className="modal-footer d-flex justify-content-center">
+              <button
+                className="align-self-center btn btn-secondary"
+                onClick={onClose}>
+                Got it
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  });
+
+  return null;
+};
+
+export default AlertMessage;
+
+// OLD PICTURES
+// "https://ctl.s6img.com/society6/img/7YUzbWKPlRNoDuosqB_FyQbfYag/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/8802652faa0d4b5a935d15a2a56436cc/~~/famous-band-walk-road-sign-design-prints.jpg"
+// "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
+// "https://www.smallbizgenius.net/wp-content/uploads/2019/11/smallbizgenius_favicon.png"
