2837a04107604d302e18e8e48f51c8ecf3cbdbb0
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-10 00:34:33 -0500

    remove uncecessary styling, replace all help images with cats as per Nat's request

--- src/components/global/AlertContent.js
+++ src/components/global/AlertContent.js
@@ -9,8 +9,8 @@ export default class AlertContent extends Component {
   helpMessage = status => {
     console.log(status);
     const currentDate = dateCreate();
-    if (currentDate - this.props.lastAlert > 70000) {
-      if (status <= 65) {
+    if (currentDate - this.props.lastAlert > 10000) {
+      if (status <= 63) {
         return (
           <div className="alertContainer">
             <div className="modal-dialog">
@@ -23,7 +23,8 @@ export default class AlertContent extends Component {
               <img
                 className="alertImg3"
                 src={
-                  "https://ctl.s6img.com/society6/img/7YUzbWKPlRNoDuosqB_FyQbfYag/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/8802652faa0d4b5a935d15a2a56436cc/~~/famous-band-walk-road-sign-design-prints.jpg"
+                  "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
+                  // "https://ctl.s6img.com/society6/img/7YUzbWKPlRNoDuosqB_FyQbfYag/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/8802652faa0d4b5a935d15a2a56436cc/~~/famous-band-walk-road-sign-design-prints.jpg"
                 }
               />
             </div>
@@ -49,7 +50,8 @@ export default class AlertContent extends Component {
               <img
                 className="alertImg3"
                 src={
-                  "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
+                  "https://i.pinimg.com/236x/fe/e9/e0/fee9e0741c5199b830273f88a6c4f8f3--coffee-cafe-cup-of-coffee.jpg"
+                  // "https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326443/a-cup-of-coffee-that-makes-people-tired.jpg?w=1155&h=1541"
                 }
               />
             </div>
@@ -72,7 +74,8 @@ export default class AlertContent extends Component {
               <img
                 className="alertImg3"
                 src={
-                  "https://www.smallbizgenius.net/wp-content/uploads/2019/11/smallbizgenius_favicon.png"
+                  "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
+                  // "https://www.smallbizgenius.net/wp-content/uploads/2019/11/smallbizgenius_favicon.png"
                 }
               />
             </div>
