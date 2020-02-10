b10c31038068d107b3757d760369a5fc1eeca829
James Shen <jjss886@gmail.com>
2020-02-10 14:04:07 -0500

    updated certain styling

--- src/components/alert/AlertMessage.js
+++ src/components/alert/AlertMessage.js
@@ -5,10 +5,9 @@ const AlertMessage = ({ status, onClose }) => {
 
   const alertOptions = {
     0: {
-      message:
-        "Woah there Buddy! Step away from your desk and put those hands up! Take 10-15 mins to yourself and walk it off",
+      message: "Here is a cute picture, Hope it makes your day!",
       image:
-        "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
+        "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
     },
     1: {
       message:
@@ -17,9 +16,10 @@ const AlertMessage = ({ status, onClose }) => {
         "https://i.pinimg.com/236x/fe/e9/e0/fee9e0741c5199b830273f88a6c4f8f3--coffee-cafe-cup-of-coffee.jpg"
     },
     2: {
-      message: "Here is a cute picture, Hope it makes your day!",
+      message:
+        "Woah there Buddy! Step away from your desk and put those hands up! Take 10-15 mins to yourself and walk it off",
       image:
-        "https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg"
+        "https://i.pinimg.com/736x/df/81/9d/df819dadcce0ce5a6929f25da03a8025--funny-stuff-funny-things.jpg"
     }
   };
 
@@ -49,36 +49,6 @@ const AlertMessage = ({ status, onClose }) => {
       );
     }
   }
-
-  return null;
-
-  tiers.forEach((tier, idx) => {
-    if (status <= tier) {
-      console.log("inside alert msg -", status);
-      return "hello!";
-      return (
-        <div className="alertBackdrop d-flex justify-content-center">
-          <div className="alertContainer">
-            <div className="modal-dialog">
-              <h6>{alertOptions[idx].message}</h6>
-            </div>
-
-            <div className="alertImgDiv">
-              <img className="alertImg3" src={alertOptions[idx].image} />
-            </div>
-
-            <div className="modal-footer d-flex justify-content-center">
-              <button
-                className="align-self-center btn btn-secondary"
-                onClick={onClose}>
-                Got it
-              </button>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  });
 
   return null;
 };
