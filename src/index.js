99fa3e3b5dbc60762a1d114d0d5db969fba24c39
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-09 21:17:51 -0500

    added all proper bootstrap dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./ast/style.css";
 import "bootstrap";
+import $ from "jquery";
 import App from "./components/App";
 import * as serviceWorker from "./ast/serviceWorker";
 import history from "./history";
