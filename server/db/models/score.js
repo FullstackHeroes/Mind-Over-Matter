ca06318d6ae9d32009c5cb13c1623aa70b154d39
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-10 12:27:04 -0500

    remove excess tables and add Weighted Score to models/ score.js

--- server/db/models/score.js
+++ server/db/models/score.js
@@ -1,85 +1,7 @@
 const Sequelize = require("sequelize");
 const { db } = require("../db");
 
-const Hour = db.define("hour", {
-  trueScore: {
-    type: Sequelize.FLOAT
-  },
-  happy: {
-    type: Sequelize.FLOAT
-  },
-  surprised: {
-    type: Sequelize.FLOAT
-  },
-  neutral: {
-    type: Sequelize.FLOAT
-  },
-  disgusted: {
-    type: Sequelize.FLOAT
-  },
-  fearful: {
-    type: Sequelize.FLOAT
-  },
-  angry: {
-    type: Sequelize.FLOAT
-  },
-  sad: {
-    type: Sequelize.FLOAT
-  },
-  timeStamp: {
-    type: Sequelize.STRING
-  },
-  count: {
-    type: Sequelize.INTEGER
-  },
-  screenScore: {
-    type: Sequelize.FLOAT
-  },
-  screenTime: {
-    type: Sequelize.FLOAT
-  }
-});
-
-const Day = db.define("day", {
-  trueScore: {
-    type: Sequelize.FLOAT
-  },
-  happy: {
-    type: Sequelize.FLOAT
-  },
-  surprised: {
-    type: Sequelize.FLOAT
-  },
-  neutral: {
-    type: Sequelize.FLOAT
-  },
-  disgusted: {
-    type: Sequelize.FLOAT
-  },
-  fearful: {
-    type: Sequelize.FLOAT
-  },
-  angry: {
-    type: Sequelize.FLOAT
-  },
-  sad: {
-    type: Sequelize.FLOAT
-  },
-  timeStamp: {
-    type: Sequelize.STRING
-  },
-  count: {
-    type: Sequelize.INTEGER
-  },
-  screenScore: {
-    type: Sequelize.FLOAT
-  },
-  screenTime: {
-    type: Sequelize.FLOAT
-  }
-});
-
-const Week = db.define("week", {
+const WeightedScore = db.define("WeightedScore", {
   trueScore: {
     type: Sequelize.FLOAT
   },
@@ -133,4 +55,4 @@ const NormalizeScore = db.define("normalizeScore", {
   }
 });
 
-module.exports = { Hour, Day, Week, NormalizeScore };
+module.exports = { WeightedScore, NormalizeScore };
