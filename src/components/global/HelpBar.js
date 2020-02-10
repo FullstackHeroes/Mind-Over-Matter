e6d9d3ecbbbd388726f590e33e1aa33ef7bb9a3a
James Shen <jjss886@gmail.com>
2020-02-10 15:26:28 -0500

    removed class name in button in help bar

--- src/components/global/HelpBar.js
+++ src/components/global/HelpBar.js
@@ -30,9 +30,7 @@ export default () => (
         <h6>My Question is: </h6>
         <textarea rows="7" cols="75"></textarea>
         <br></br>
-        <button type="submit" class="submit">
-          Submit Question
-        </button>
+        <button type="submit">Submit Question</button>
       </form>
     </div>
 
