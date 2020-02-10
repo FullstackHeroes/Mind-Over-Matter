59665022cdefba89d99e008e460a43328bd1876c
James Shen <jjss886@gmail.com>
2020-02-10 15:47:30 -0500

    refactoring css for alert

--- src/components/alert/AlertMessage.js
+++ src/components/alert/AlertMessage.js
@@ -29,19 +29,19 @@ const AlertMessage = ({ status, onClose, last }) => {
       return (
         <div className="alertFullDiv">
           <div className="alertContainer">
-            <div className="modal-dialog">
-              <h6>{alertOptions[i].message}</h6>
-            </div>
+            {/* <div className="modal-dialog"> */}
+            <h6>{alertOptions[i].message}</h6>
+            {/* </div> */}
 
-            <div className="alertImgDiv">
-              <img className="alertImg3" src={alertOptions[i].image} />
-            </div>
+            {/* <div className="alertImgDiv"> */}
+            <img className="alertImg3" src={alertOptions[i].image} />
+            {/* </div> */}
 
-            <div className="modal-footer d-flex justify-content-center">
-              <button className="alertBtn" onClick={onClose}>
-                Got it
-              </button>
-            </div>
+            {/* <div className="modal-footer d-flex justify-content-center"> */}
+            <button className="alertBtn" onClick={onClose}>
+              Got it
+            </button>
+            {/* </div> */}
           </div>
         </div>
       );
