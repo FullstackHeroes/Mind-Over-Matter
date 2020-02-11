dcb2e121b3502616807bab0a9233429c1df4d675
Kenneth Chen <kennethchen91@gmail.com>
2020-02-11 16:36:42 -0500

    saving progress for merge

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import HelpBar from "./global/HelpBar";
+import zipcodes from "zipcodes";
 
 const Doctor = () => (
   <div className="dashboardFullDiv">
@@ -27,6 +28,8 @@ const Doctor = () => (
     <HelpBar />
   </div>
 );
+
+const lookupZip = zip => {};
 
 export default Doctor;
 
