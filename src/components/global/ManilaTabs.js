415851e199c059ea8d90f3ea9c7a1ea31db0f9f6
Emmanuel Kandilas <Ekandilas@gmail.com>
2020-02-11 17:10:15 -0500

    basic layout for manila tabs

--- src/components/global/ManilaTabs.js
+++ src/components/global/ManilaTabs.js
@@ -1,91 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-// import logo from "../../images/logo.svg";
-import { logout } from "../../store";
 
-class NavBar extends Component {
-  loggingOut = () => {
-    this.props.logout();
-  };
-
+export default class ManilaTab extends Component {
   render() {
-    const userId = this.props.user.id;
-
     return (
       <div className="navBarFullDiv">
-        <div className="navBarLeft">
-          <h1 className="navBarHeader">Mind Over Matter</h1>
-          {/* <img src={logo} className="appLogo" alt="logo" /> */}
+        <div className="navBarRight">
+          <NavLink
+            to="/Dashboard"
+            className="linkText navBarLink"
+            activeClassName="selectedNavLink"
+            exact>
+            Dashboard
+          </NavLink>
+
+          <NavLink
+            to="/Table"
+            className="linkText navBarLink"
+            activeClassName="selectedNavLink"
+            exact>
+            Table
+          </NavLink>
         </div>
-
-        {userId ? (
-          <div className="navBarRight">
-            <NavLink
-              to="/"
-              className="linkText navBarLink"
-              activeClassName="selectedNavLink"
-              exact>
-              HomePage
-            </NavLink>
-
-            <NavLink
-              to="/Dashboard"
-              className="linkText navBarLink"
-              activeClassName="selectedNavLink"
-              exact>
-              Dashboard
-            </NavLink>
-
-            <NavLink
-              to="/Table"
-              className="linkText navBarLink"
-              activeClassName="selectedNavLink"
-              exact>
-              Table
-            </NavLink>
-
-            <a
-              href="#"
-              onClick={this.loggingOut}
-              className="linkText navBarLink">
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div className="navBarRight">
-            <NavLink
-              to="/"
-              className="linkText navBarLink"
-              activeClassName="selectedNavLink"
-              exact>
-              HomePage
-            </NavLink>
-
-            <NavLink
-              to="/SignIn"
-              className="linkText navBarLink"
-              activeClassName="selectedNavLink"
-              exact>
-              Sign In
-            </NavLink>
-          </div>
-        )}
       </div>
     );
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    user: state.user
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    logout: () => dispatch(logout())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
