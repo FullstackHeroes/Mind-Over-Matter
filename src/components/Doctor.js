7831f58c9eb9892f8674fb580b971ab2dd143d97
Kenneth Chen <kennethchen91@gmail.com>
2020-02-11 23:45:45 -0500

    saving progress for merge

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -11,23 +11,106 @@ const Doctor = () => (
 
     <form className="doctorForm">
       <label>
-        <small>Enter Your Zip Code: </small>
+        <small>Enter Your Zip Code</small>
         <input
           type="text"
-          class="form-control"
+          id="zipInput"
+          className="form-control"
           placeholder="Zip Code"
           maxLength="5"></input>
       </label>
       <br></br>
-      <input className="btn btn-dark" type="search" value="Search"></input>
+      <button type="button" className="btn btn-dark">
+        Search
+      </button>
     </form>
+    <br></br>
 
-    <div className="doctorsList"></div>
+    <div className="doctorsList">
+      <div class="row">
+        <div class="col-4">
+          <div class="list-group" id="list-tab" role="tablist">
+            <a
+              class="list-group-item list-group-item-action active"
+              id="list-home-list"
+              data-toggle="list"
+              href="#list-home"
+              role="tab"
+              aria-controls="home">
+              Doctor 1
+            </a>
+            <a
+              class="list-group-item list-group-item-action"
+              id="list-profile-list"
+              data-toggle="list"
+              href="#list-profile"
+              role="tab"
+              aria-controls="profile">
+              Doctor 2
+            </a>
+            <a
+              class="list-group-item list-group-item-action"
+              id="list-messages-list"
+              data-toggle="list"
+              href="#list-messages"
+              role="tab"
+              aria-controls="messages">
+              Doctor 3
+            </a>
+            <a
+              class="list-group-item list-group-item-action"
+              id="list-settings-list"
+              data-toggle="list"
+              href="#list-settings"
+              role="tab"
+              aria-controls="settings">
+              Doctor 4
+            </a>
+          </div>
+        </div>
+        <div class="col-8">
+          <div class="tab-content" id="nav-tabContent">
+            <div
+              class="tab-pane fade show active"
+              id="list-home"
+              role="tabpanel"
+              aria-labelledby="list-home-list">
+              Doctor 1 Info
+            </div>
+            <div
+              class="tab-pane fade"
+              id="list-profile"
+              role="tabpanel"
+              aria-labelledby="list-profile-list">
+              Doctor 2 Info
+            </div>
+            <div
+              class="tab-pane fade"
+              id="list-messages"
+              role="tabpanel"
+              aria-labelledby="list-messages-list">
+              Doctor 3 Info
+            </div>
+            <div
+              class="tab-pane fade"
+              id="list-settings"
+              role="tabpanel"
+              aria-labelledby="list-settings-list">
+              Doctor 4 Info
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <div></div>
     <HelpBar />
   </div>
 );
+
+function getZipCode() {
+  const zipCode = document.getElementById("zipInput").value;
+}
 
 const lookupZip = zip => {};
 
