eaa21d1edab4c2122896f8969ad1fb72ec8f7cbf
Robert Costello <robertcostello81@gmail.com>
2020-02-13 16:19:10 -0500

    open articles in new tab

--- src/components/global/Article.js
+++ src/components/global/Article.js
@@ -4,7 +4,7 @@ const Article = ({ article, idx }) => (
   <div className="articleFullDiv">
     <h4 className="articleListText">
       {idx}.&nbsp;&nbsp;&nbsp;
-      <a href={article.url} className="articleListText">
+      <a href={article.url} target="_blank" className="articleListText">
         {article.title.length > 85
           ? `${article.title.slice(0, 85)}...`
           : article.title}
